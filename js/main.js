c91a649900e8db8e59b88476cea8e5054ab11a1b
EC2 Default User <ec2-user@ip-172-31-53-37.ec2.internal>
2020-07-17 16:48:50 +0000

    vue-js-modalを導入、キープ機能のバグ修正

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 Vue.component('paginate', VuejsPaginate)
+const VModal = window["vue-js-modal"].default
+
+Vue.use(VModal);
 
 /***************************************************************
  ** Vueインスタンスの生成
@@ -48,7 +51,7 @@ var app = new Vue({
                 this.marker[index].setMap(map);
 
                 //キープ機能用変数にfalseを代入する
-                this.keep[index] = false;
+                this.$set(this.keep, index, false);
             });
         }
     },
@@ -59,6 +62,14 @@ var app = new Vue({
         // ページネーションクリック時にcurrentPageを更新
         clickCallback: function(pageNum) {
             this.currentPage = Number(pageNum);
+        },
+
+        // モーダル
+        show: function(item) {
+            this.$modal.show(item);
+        },
+        hide: function(item) {
+            this.$modal.hide(item);
         },
 
         // スクロールイベント
@@ -74,32 +85,44 @@ var app = new Vue({
                 behavior: "smooth"
             });
         },
+        handleScroll() {
+            this.scrollY = window.scrollY
+        },
+
+        // キープ機能
         setKeep: function(event, index) {
-            this.$set(this.keep, index, !this.keep[index]);
+            var item_index = index + (this.currentPage - 1) * 10;
+
+            this.$set(this.keep, item_index, !this.keep[item_index]);
             console.log(this.keep[index]);
             event.target.classList.toggle("keep");
+            if (this.keep[item_index] === true) {
+                // キープしたとき
 
-            if (this.keep[index] === true) {
-                this.marker[index].setMap(null);
-                this.marker[index] = new google.maps.Marker({
-                    position: new google.maps.LatLng(this.result.rest[index].latitude, this.result.rest[index].longitude),
-                    title: this.result.rest[index].name,
-                    icon:{
-                        url:"https://lh3.googleusercontent.com/djp9vfJHgjgd4NE1F7P1GoCGYc034PeW2iZAldpuvtsexX1i0meGaQLBhqK4LRRnJy2-PZD1ZFUjuN9BZj5wk8dry75H2eirFW60Qbg1vHe0WexAu0o-nKYjdiv7FPNgHiQv8aFSnRVG9xN-EFGCe4MKgSEkmeVBrdGBdix8lnATRB3LuS3h1tEwtDG1m_YLC-_meF8Eoc9TfeSO3-UUv6rTwvVDHqLG52W7Wqfb_olrSjzO-CMWhkI44GBj6dN6dp0reJQscgyWLd6boYl1tPuvmdQxYRW9weqqS-rol8RcSqzEZtck_nzztC8vALJ5BdUjCAaQ4clRB4O0R-P81XkWR_kfPX2tra3dfZ-8K5_gxolOal6o2G3Mitxt2fJsUggo-MnU36F-eVkylAcwYge_Z3BzSSuWoN1nOH1K0MC9fr_-FuJYbXlTX_oppVuK3EXgcy7IgyEM1oQ331vss8jlS4PJaoo44VL3hgvhVQashJThFj4yCT9g-ekARuvPYL3ZjPzzdtUQrRfFNmv1Lscs8iDNdiG3A9plqY9hiW2EiycJeaM9W0yPG6effmAaT9p4QtzpvkPEOb-qFEqUn6rqCYx6vqLh2u9E3c1Z7eym08iEWdJtMtORvdgfSjfyD8D3YAryJTUjcF9ZohL5k9dWRvKZHWJA_3rgxwYz5gYvf3JtMoA-GtxYpzTIEA=s256-no?authuser=0",
+                // マーカーを変える
+                this.marker[item_index].setMap(null);
+                this.marker[item_index] = new google.maps.Marker({
+                    position: new google.maps.LatLng(this.result.rest[item_index].latitude, this.result.rest[item_index].longitude),
+                    title: this.result.rest[item_index].name,
+                    icon: {
+                        url: "https://lh3.googleusercontent.com/djp9vfJHgjgd4NE1F7P1GoCGYc034PeW2iZAldpuvtsexX1i0meGaQLBhqK4LRRnJy2-PZD1ZFUjuN9BZj5wk8dry75H2eirFW60Qbg1vHe0WexAu0o-nKYjdiv7FPNgHiQv8aFSnRVG9xN-EFGCe4MKgSEkmeVBrdGBdix8lnATRB3LuS3h1tEwtDG1m_YLC-_meF8Eoc9TfeSO3-UUv6rTwvVDHqLG52W7Wqfb_olrSjzO-CMWhkI44GBj6dN6dp0reJQscgyWLd6boYl1tPuvmdQxYRW9weqqS-rol8RcSqzEZtck_nzztC8vALJ5BdUjCAaQ4clRB4O0R-P81XkWR_kfPX2tra3dfZ-8K5_gxolOal6o2G3Mitxt2fJsUggo-MnU36F-eVkylAcwYge_Z3BzSSuWoN1nOH1K0MC9fr_-FuJYbXlTX_oppVuK3EXgcy7IgyEM1oQ331vss8jlS4PJaoo44VL3hgvhVQashJThFj4yCT9g-ekARuvPYL3ZjPzzdtUQrRfFNmv1Lscs8iDNdiG3A9plqY9hiW2EiycJeaM9W0yPG6effmAaT9p4QtzpvkPEOb-qFEqUn6rqCYx6vqLh2u9E3c1Z7eym08iEWdJtMtORvdgfSjfyD8D3YAryJTUjcF9ZohL5k9dWRvKZHWJA_3rgxwYz5gYvf3JtMoA-GtxYpzTIEA=s256-no?authuser=0",
                         scaledSize: new google.maps.Size(43, 43)
                     }
                 });
 
                 // マーカーを地図中にセットする
-                this.marker[index].setMap(map);
+                this.marker[item_index].setMap(map);
             }
             else {
-                this.marker[index].setMap(null);
-                this.marker[index] = new google.maps.Marker({
-                    position: new google.maps.LatLng(this.result.rest[index].latitude, this.result.rest[index].longitude),
-                    title: this.result.rest[index].name,
+                // キープを外すとき
+
+                // マーカーを変える
+                this.marker[item_index].setMap(null);
+                this.marker[item_index] = new google.maps.Marker({
+                    position: new google.maps.LatLng(this.result.rest[item_index].latitude, this.result.rest[item_index].longitude),
+                    title: this.result.rest[item_index].name,
                     label: {
-                        text: String(index + 1),
+                        text: String(item_index + 1),
                         color: "#fff",
                         fontWeight: 'bold',
                         fontSize: '14px'
@@ -107,11 +130,8 @@ var app = new Vue({
                 });
 
                 // マーカーを地図中にセットする
-                this.marker[index].setMap(map);
+                this.marker[item_index].setMap(map);
             }
-        },
-        handleScroll() {
-            this.scrollY = window.scrollY
         },
     },
     computed: {
