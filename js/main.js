c40d8a5a217bebb495b49668b2d5de4e4e10723f
EC2 Default User <ec2-user@ip-172-31-53-37.ec2.internal>
2020-07-18 18:50:56 +0000

    条件検索機能を追加

--- js/main.js
+++ js/main.js
@@ -7,13 +7,22 @@ Vue.component('paginate', VuejsPaginate)
 var app = new Vue({
     el: "#app",
     data: {
+        // 条件検索設定
+        rengeNum: "",
+        isCard: false,
+        isE_money: false,
+        isParking: false,
+        isWifi: false,
+        freeInput: "",
+        url: "result.html?",
+
         // グーグルマップ設定
         lat: 35.6809591,
         lng: 139.7673068,
         marker: {},
 
         // グルナビ設定
-        range: 5,
+        range: 2,
         wifi: 0,
         parking: 0,
         e_money: 0,
@@ -27,12 +36,6 @@ var app = new Vue({
         currentPage: 1,
 
         scrollY: 0
-    },
-    components: {
-        Hooper: window.Hooper.Hooper,
-        Slide: window.Hooper.Slide,
-        HooperNavigation: window.Hooper.HooperNavigation,
-        HooperPagination: window.Hooper.HooperPagination
     },
     watch: {
         // この関数は result が変わるごとに実行される
@@ -56,7 +59,62 @@ var app = new Vue({
                 //キープ機能用変数にfalseを代入する
                 this.$set(this.keep, index, false);
             });
+        },
+    },
+    created: function() {
+        // 検索パラメータを受け取る処理
+
+        // アドレスの「?」以降を受け取る
+        var pram = location.search;
+
+        // 引数がない場合は処理しない
+        if (!pram) return "";
+
+        /* 先頭の?,&をカット */
+        pram = pram.substring(2);
+        console.log(pram);
+
+        // 「&」で引数を分割して配列にする
+        var pair = pram.split("&");
+        console.log(pair);
+        var temp = "";
+
+        for (let i = 0; i < pair.length; i++) {
+            // 配列の値を「=」で分割
+            temp = pair[i].split("=");
+            console.log(pair);
+
+            // パラメータ名、パラメータ値
+            let keyName = temp[0];
+            let keyValue = temp[1];
+
+            // パラメータ名に応じてdataプロパティに入力
+            switch (keyName) {
+                case "range":
+                    this.range=Number(keyValue);
+                    break;
+                case "card":
+                    this.card=Number(keyValue);
+                    break;
+                case "e_money":
+                    this.e_money=Number(keyValue);
+                    break;
+                case "parking":
+                    this.parking=Number(keyValue);
+                    break;
+                case "wifi":
+                    this.wifi=Number(keyValue);
+                    break;
+                case "freeword":
+                    // デコード
+                    var word = decodeURIComponent(keyValue)
+                    this.freeword=word;
+                    break;
+            }
+
         }
+
+
     },
     mounted() {
         window.addEventListener('scroll', this.handleScroll)
@@ -65,14 +123,6 @@ var app = new Vue({
         // ページネーションクリック時にcurrentPageを更新
         clickCallback: function(pageNum) {
             this.currentPage = Number(pageNum);
-        },
-
-        // モーダル
-        show: function(item) {
-            this.$modal.show(item);
-        },
-        hide: function(item) {
-            this.$modal.hide(item);
         },
 
         // スクロールイベント
@@ -136,6 +186,25 @@ var app = new Vue({
                 this.marker[item_index].setMap(map);
             }
         },
+
+        // 条件検索用URL作成
+        getUrl: function() {
+            // rengeパラメータをurlに追加
+            if (this.rengeNum !== "") this.url += `&range=${this.rengeNum}`;
+            // cardパラメータをurlに追加
+            if (this.isCard) this.url += "&card=1";
+            // e_moneyパラメータをurlに追加
+            if (this.isE_money) this.url += "&e_money=1";
+            // parkingパラメータをurlに追加
+            if (this.isParking) this.url += "&parking=1";
+            // Wifiパラメータをurlに追加
+            if (this.isWifi) this.url += "&wifi=1";
+            // freewordパラメータをurlに追加
+            if (this.freeInput !== "") this.url += `&freeword=${this.freeInput}`;
+
+            // URLに遷移
+            window.location.href = this.url;
+        }
     },
     computed: {
         // 現在ページのアイテムを返す
